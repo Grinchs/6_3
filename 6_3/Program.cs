fa50d4a157a44c70c587d7e0795372c18694d6d8
Grinchs <marcisg2345@gmail.com>
2023-12-19 14:16:24 +0200

    Add project files.

---
+++ 6_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ievadiet skaitli R: ");
+            int R = int.Parse(Console.ReadLine());
+            Console.Write("Ievadiet pirmo skaitli: ");
+            int pirmais_sk = int.Parse(Console.ReadLine());
+            int i = 0;
+
+            while (i <= R)
+            {
+                if (i == R)
+                {
+                    Console.WriteLine($"Reiz[{i}] = {pirmais_sk}");
+                    break;
+                }
+
+                Console.Write($"Reiz[{i}] = {pirmais_sk}*");
+                int otrais_sk = int.Parse(Console.ReadLine());
+                pirmais_sk = pirmais_sk * otrais_sk;
+                i++;
+            }
+        }
+    }
+}
